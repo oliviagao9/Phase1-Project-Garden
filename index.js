8929dbf253e84c7b2d44094ca951348f05fccff4
oliviagao9 <oliviagao9@gmail.com>
2023-10-04 19:34:17 -0500

    update logo styling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => fetchData)
 
 function fetchData() {
-    fetch('http://localhost:3000')
+    fetch('http://localhost:3000/plants')
     .then((resp) => resp.json())
     .then((data) => console.log(data));
 }
