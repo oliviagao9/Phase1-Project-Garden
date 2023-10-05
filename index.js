081c2c7d078dcdbb8a8aafb7ea807c8e9380dc11
oliviagao9 <oliviagao9@gmail.com>
2023-10-04 21:39:24 -0500

    renderPlants

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => fetchData)
+document.addEventListener("DOMContentLoaded", () => displayPlants())
 
-function fetchData() {
+function displayPlants() {
     fetch('http://localhost:3000/plants')
-    .then((resp) => resp.json())
-    .then((data) => console.log(data));
+        .then((resp) => resp.json())
+        .then((plants) => plants.forEach(plant => renderPlant(plant)));
 }
+
+function renderPlant(plant) {
+
+    let card = document.createElement('li');
+    card.className = 'card'
+    let img = document.createElement('img');
+    img.src = plant.image;
+    card.appendChild(img);
+    card.innerHTML = card.innerHTML + `<div class="container"> ${plant.name} </div>`;
+
+    console.log(card);
+    
+    document.getElementById('cards').appendChild(card);
+};
