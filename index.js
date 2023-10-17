ac4db721cc8e7c4133f7a9099dd5fa382f0b35d4
oliviagao9 <oliviagao9@gmail.com>
2023-10-16 23:33:16 -0500

    added search function and listener and cleaned up

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+let plantList = [];
+
 document.addEventListener("DOMContentLoaded", () => {
 
 displayPlants()
 addPlantFormSubmitListener()
+addSearchListener()
 
 })
 
 function displayPlants() {
     fetch('http://localhost:3000/plants')
         .then((resp) => resp.json())
-        .then((plants) => plants.forEach(plant => renderPlant(plant)));
+        .then((plants) => {
+            return plants.forEach(plant =>  {
+                plantList.push(plant);
+                return renderPlant(plant);
+        })});
 }
 
 function renderPlant(plant) {
@@ -18,22 +25,54 @@ function renderPlant(plant) {
     let img = document.createElement('img');
     img.src = plant.image;
     card.appendChild(img);
-    card.innerHTML = card.innerHTML + 
-    `<div class="container"> 
-     <h3>${plant.name}</h3> 
-     <h4>☀️${plant.sunlight}</h4></div>`;
-    
+
+    card.innerHTML = card.innerHTML +
+        `<div class="container"> 
+            <h3>${plant.name}</h3> 
+            <h4>☀️${plant.sunlight}</h4>
+        </div>`;
+
     document.getElementById('cards').appendChild(card);
 };
 
-function addPlantFormSubmitListener() {
-    const plantForm = document.getElementById("plant-form");
 
+function addPlantFormSubmitListener() {
+    const plantForm = document.querySelector("form");
     plantForm.addEventListener("submit", (e) => {
         e.preventDefault();
         addNewPlant();
     })
 }
+
+function resetPlantCardDom() {
+    document.getElementById('cards').innerHTML = "";
+}
+
+function addSearchListener() {
+    const searchInput = document.getElementById("searchBar");
+
+    searchInput.addEventListener("keyup", (event) => {
+        let searchString = event.target.value;
+
+        if (searchString == "") {
+            resetPlantCardDom() 
+            return plantList.forEach(plant => renderPlant(plant));
+        }
+
+        let filteredPlant = plantList.filter( plant => {
+
+            let includedPlant = plant.name.toLowerCase()
+                    .includes(searchString.toLowerCase())   
+
+            return includedPlant;
+            
+        })
+
+        resetPlantCardDom();
+        filteredPlant.forEach(plant => renderPlant(plant));
+    });
+}
+
 
 function addNewPlant() {
     const newName = document.getElementById("new-name").value;
@@ -46,14 +85,16 @@ function addNewPlant() {
         "sunlight": newSunlight
     }
 
-    fetch("http://localhost:3000/plants", {
+    fetch('http://localhost:3000/plants', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify(newPlant)
+        body:JSON.stringify(newPlant)
     })
 
+    plantList.push(newPlant);
 
     renderPlant(newPlant);
 }
+
